0d88a1a7671a663e03ed196f8ecd55d1dead2836
yeomsoo <jakoe3@gmail.com>
2023-05-02 09:26:43 +0900

    feat: Add element in list

--- new_main.py
+++ new_main.py
@@ -11,6 +11,7 @@ def print_hello():
             'Jane', 
             'Gil-dong',
             'Dong-eun',
+            'Yeon-jin',
     ] # w/ trailing comma
     for f_name in names:
         print(f'hello, {f_name}')
